3cc5cd2a4bea52dfa6aab501031aa325ddf2472c
Kacp_r N <kacp.r.nalepa@gmail.com>
2025-08-12 09:32:24 +0200

    created shader.h

--- src/shader.h
+++ src/shader.h
@@ -2,4 +2,28 @@
 
 #include "glad/glad.h"
 #include "GLFW/glfw3.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
 
+class Shader
+{
+    private:
+    GLuint VAO;
+    GLuint VBO;
+    GLuint Program_ID;
+
+    std::string frag_Path;
+    std::string vert_Path;
+
+    public:
+    Shader(const std::string& vertexShaderPath, const std::string& fragmentShaderPath);
+    ~Shader();
+    void loadShader(const std::string& Path, GLuint ShaderID);
+    void compileShader(GLuint ShaderID);
+    void createProgram();
+    void use();
+
+    GLuint getProgramID() const;
+};
